1009ccb03169e915856ef8fd9626ea9b5d39ba4a
Sachen Sampath <scsampath@ucsb.edu>
2021-11-25 01:21:20 -0800

    Initial Commit

---
+++ app/src/main/java/com/example/gauchomap/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.gauchomap;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
