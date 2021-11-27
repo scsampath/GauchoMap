be82e8664d881d5af716372bca19e07a87b1d461
Sachen Sampath <scsampath@ucsb.edu>
2021-11-26 18:33:49 -0800

    Added Map Fragment and Animation. Marker for current location working.

--- app/src/main/java/com/example/gauchomap/MainActivity.java
+++ app/src/main/java/com/example/gauchomap/MainActivity.java
@@ -1,14 +1,122 @@
 package com.example.gauchomap;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.fragment.app.FragmentContainerView;
 
+import android.Manifest;
+import android.annotation.SuppressLint;
+import android.app.AlertDialog;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.SharedPreferences;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.os.Build;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import com.airbnb.lottie.LottieAnimationView;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+import android.animation.Animator;
+import android.os.Handler;
+import android.util.Log;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity implements LocationListener, OnMapReadyCallback {
+
+    private LottieAnimationView animationView;
+    private GoogleMap googleMap;
+
+    private LocationManager mLocationManager;
+
+    private Marker currentLocation;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        animationView = findViewById(R.id.animationView);
+
+        mLocationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return;
+        }
+        mLocationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 1000, 0, this);
+
+        final Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                // Set up Google Maps
+                SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                        .findFragmentById(R.id.map);
+                mapFragment.getMapAsync(MainActivity.this::onMapReady);
+            }
+        }, 3000);
     }
+
+    @Override
+    public void onLocationChanged(Location location) {
+        Log.e("Location: ","Location Changed");
+
+        if(currentLocation != null){
+            currentLocation.remove();
+        }
+
+        final LatLng loc = new LatLng(location.getLatitude(), location.getLongitude());
+
+        currentLocation = googleMap.addMarker(new MarkerOptions()
+                .position(loc)
+                .icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_AZURE))
+                .title("Current Location"));
+
+        //auto-centering
+        googleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(loc, 15));
+    }
+
+    @Override
+    protected void onStart() throws SecurityException {
+        super.onStart();
+
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        FragmentContainerView mapView = findViewById(R.id.map);
+        mapView.setVisibility(View.VISIBLE);
+        animationView.setVisibility(View.INVISIBLE);
+        this.googleMap = googleMap;
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        mLocationManager.removeUpdates(this);
+    }
+
 }
